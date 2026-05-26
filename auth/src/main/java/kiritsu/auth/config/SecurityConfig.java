78afe21113f8fb14c6d5e4b35c30bdfc99fdd9d7
MahmoodMI1 <mahmood.idelbi1@icloud.com>
2026-05-26 12:14:53 -0400

    chore: added a base security configuration and default login

--- auth/src/main/java/kiritsu/auth/config/SecurityConfig.java
+++ auth/src/main/java/kiritsu/auth/config/SecurityConfig.java
@@ -1,4 +1,35 @@
 package kiritsu.auth.config;
 
+import kiritsu.auth.UserRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
 public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) {
+        return http
+                .authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
+
+                .formLogin(Customizer.withDefaults())
+
+                .build();
+    }
+
+    @Bean
+    UserDetailsService userDetailsService (UserRepository userRepository) {
+        return userRepository::findByUsername;
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder () {
+        return new BCryptPasswordEncoder();
+    }
 }
